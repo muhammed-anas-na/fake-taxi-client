b3ccb58561d45fbc58f984513a3cfe4f0a6d3308
Anas <anasna6005@gmail.com>
2024-01-16 00:25:08 +0530

    Back button issue fixed for driver

--- src/Routes/DriverRoutes.tsx
+++ src/Routes/DriverRoutes.tsx
@@ -4,16 +4,30 @@ import Signup from "../Components/Driver/Signup";
 import EnterVhDetails from "../Components/Driver/EnterVhDetails";
 import DriverHome from "../Components/Driver/DriverHome";
 import Otp from "../Components/Driver/Otp";
+import ProtectedAuth from "./ProtectAuth";
 
-export default function DriverRoutes(){
-    return(
-        <Routes>
-            <Route path="/" element={<DriverHome/>}/>
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/signup" element={<Signup/>}/>
-            <Route path="/otp" element={<Otp/>}/>
-            <Route path="/vehicle_details" element={<EnterVhDetails/>}/>
-            
-        </Routes>
-    )
+export default function DriverRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<DriverHome />} />
+      <Route
+        path="/login"
+        element={
+          <ProtectedAuth>
+            <Login />
+          </ProtectedAuth>
+        }
+      />
+      <Route path="/signup" element={<Signup />} />
+      <Route
+        path="/otp"
+        element={
+          <ProtectedAuth>
+            <Otp />
+          </ProtectedAuth>
+        }
+      />
+      <Route path="/vehicle_details" element={<EnterVhDetails />} />
+    </Routes>
+  );
 }
