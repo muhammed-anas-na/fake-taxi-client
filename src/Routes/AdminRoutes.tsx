313594ddb64434fc60a6d4edf0ea717c9fc76235
Anas <anasna6005@gmail.com>
2024-01-16 00:26:24 +0530

    back button issue after login fixed for admin

--- src/Routes/AdminRoutes.tsx
+++ src/Routes/AdminRoutes.tsx
@@ -1,12 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import AdminLogin from "../Components/Admin/AdminLogin";
 import AdminDashboard from '../Components/Admin/AdminDashboard'
+import ProtectedAuth from "./ProtectAuth";
 
 export default function AdminRoutes(){
     return(
         <Routes>
             <Route path="/" element={<AdminDashboard/>}/>
-            <Route path="/login" element={<AdminLogin/>}/>
+            <Route path="/login" element={
+            <ProtectedAuth>
+            <AdminLogin/>
+            </ProtectedAuth>
+            }/>
         </Routes>
     )
 }
